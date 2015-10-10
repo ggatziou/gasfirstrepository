6d442c4c48223ba04d2dfd0ef4982ebbb817493e
ggatziou <ggatziou@gmail.com>
2015-10-11 00:36:30 +0300

    11/10

--- gasfirstproject/gasfirstprojectforgit/src/gasfirstprojectforgit/HelloWorld.java
+++ gasfirstproject/gasfirstprojectforgit/src/gasfirstprojectforgit/HelloWorld.java
@@ -5,9 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//commit1
+		//commit3
 		//commit2
 		int gg=1;
-
+		int ga=2;
 	}
 
 }
