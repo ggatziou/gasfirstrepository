a82e77c6aadc0db5c72d52e4ba3a5155837759b0
ggatziou <ggatziou@gmail.com>
2015-10-10 03:24:44 +0300

    c1

--- gasfirstproject/gasfirstprojectforgit/src/gasfirstprojectforgit/HelloWorld.java
+++ gasfirstproject/gasfirstprojectforgit/src/gasfirstprojectforgit/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		//commit1
 		//commit2
-		
+		int gg=1;
 
 	}
 
---
+++ homeWallet/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<h3>This is a test 1</h3>
+</body>
+</html>
